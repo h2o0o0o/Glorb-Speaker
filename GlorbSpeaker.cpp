dcd1a13a6b0d0fbf256ac7b7594b40248968220a
H20 <117472913+h2o0o0o@users.noreply.github.com>
2026-01-21 07:42:18 -0800

    0.5

--- GlorbSpeaker.cpp
+++ GlorbSpeaker.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-//glorb speaker 0.3
+//glorb speaker 0.5
 //jopa
 
 void printGlorb(string filename);
@@ -24,27 +24,41 @@ int main(){
     vector<string> voice = {"null", "Frodge", "Max", "Dimak", "Old Dimak"}; 
     int n{};
 
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 2; i++){
         cout << endl;
     }
 
     int vc_size = voice.size();
 
     cout << "Helo this is Glorb Speaker, please choose your voice" << endl;
+
     for (int i = 1; i < vc_size; i++){
         cout << setfill ('-');
         cout << left << setw(13)<< voice[i]<< right << setw(5) << i << endl;
     }
 
-    cout << "Enter here: ";
-    cin >> n;
-    while (n > vc_size - 1){
-        cout << "eblan\n";
-        cout << "Enter again : ";
+    bool answ = false;
+    char c;
+
+    do {
+        cout << "> Enter here: ";
         cin >> n;
-    }
-    cout << "You choosed : " << voice[n] << endl;
+
+        while (n > vc_size - 1){
+            cout << "eblan fuck you\n";
+            cout << "> Enter again : ";
+            cin >> n;
+        }
+
+        cout << "You choosed : " << voice[n] << ", continue with " << voice[n] << "? " << voice[n] << endl;
+        cout << "y/n > "; 
+
+        cin >> c;
+        if( c == 'y' || c == 'Y') answ = true;
+        else answ = false; 
+    } while (!answ);
     
+
     system("pause");
 
 }
